e0aa439aa29340b3693948574c9186b6752d15ef
Victor Chen <victor.yccchen1@gmail.com>
2024-08-04 17:17:06 +0800

    refactor: improve code organization and readability
    This commit refactors the main.py file to improve code organization and readability. Functions for checking HP and MP, healing, attacking, and focusing on the game are extracted for better modularity. The play() function now handles the game logic in a more structured manner. This refactoring enhances maintainability and makes the code easier to understand and modify in the future.

--- main.py
+++ main.py
@@ -3,49 +3,74 @@ import pygetwindow as gw
 import time
 from PIL import ImageGrab
 import pytesseract
+import queue
 
+job_queue = queue.Queue()
 game_window = None
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+hp_x, hp_y = 300, 790
+mp_x, mp_y = 430, 790
 
 def initialize_window():
     global game_window
     game_window = gw.getWindowsWithTitle('MapleRoyals May 31 2024 IMG')[0]
 
-def check_hp():
-    bbox = (game_window.left, game_window.top, game_window.right, game_window.bottom)
-    screenshot = ImageGrab.grab(bbox)
-    hp_region = screenshot.crop((100, 100, 200, 120))
-    hp_text = pytesseract.image_to_string(hp_region, config='--psm 7')
-    try:
-        hp_value = int(hp_text.strip())
-    except ValueError:
-        hp_value = 0
-    return hp_value
+def is_blue(x, y):
+    screenshot = ImageGrab.grab()
+    pixel_color = screenshot.getpixel((x, y))
+    red, green, blue = pixel_color
+    return blue > 200
 
-def check_mp():
-    return 50
+def is_red(x, y):
+    screenshot = ImageGrab.grab()
+    pixel_color = screenshot.getpixel((x, y))
+    red, green, blue = pixel_color
+    return red > 200
 
-def heal():
-    return
+def check_hp(x, y):
+    if not is_red(x, y):
+        pydirectinput.press('t')
+
+def check_mp(x, y):
+    if not is_blue(x, y):
+        pydirectinput.press('t')
+
+def check_status():
+    pydirectinput.press('a')
+    time.sleep(1)
+    pydirectinput.press('s')
+    time.sleep(1)
+
+def find_monsters():
+    pass
 
 def attack():
+    # only when the monster is in range
     pydirectinput.press('c')
 
 def focus_on_game():
     game_window.activate()
 
 def play():
-    initialize_window()
-    time.sleep(2)
-    while True:
-        hp = check_hp()
-        print(hp)
-        # mp = check_mp()
-        # if hp < 50 or mp < 50:
-        #     heal()
-        # attack()
-        time.sleep(1)
+    job_queue.put(lambda: check_status())
+    counter = 0
+    while True: # add a command queue
+        job_queue.put(lambda: check_hp(hp_x, hp_y))
+        job_queue.put(lambda: check_mp(mp_x, mp_y))
+        counter += 1
+        if counter >= 200:
+            job_queue.put(lambda: check_status())
+            counter = 0
+        job_queue.put(lambda: attack())
+
+        job = job_queue.get()
+        job()
+        job_queue.task_done()
 
 def main():
+    initialize_window()
+    focus_on_game()
+    time.sleep(2)
     try:
         play()
     except Exception as e:
