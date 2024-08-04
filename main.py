0ac83a4ff07a053d851515c7f8bf5fdf446685c5
Victor Chen <victor.yccchen1@gmail.com>
2024-08-04 14:45:42 +0800

    feature: successfully input key in mapleroyal game

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-# hello_world.py
-import pyautogui
+import pydirectinput
+import pygetwindow as gw
 import time
 
 def main():
-    print("Hello, World!")
-    time.sleep(2)  # Wait for 2 seconds before pressing the key
-    pyautogui.press('v')  # Simulate pressing the 'v' key
+    try:
+        window = gw.getWindowsWithTitle('MapleRoyals May 31 2024 IMG')[0]
+        window.activate()
+        time.sleep(2)
+        pydirectinput.press('c')
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
